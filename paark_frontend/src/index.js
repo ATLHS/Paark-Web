5af8dc1ce688c20f4efc202f87751e6b7e1452ce
ATLH <s-attilah@hotmail.com>
2022-01-21 14:19:16 +0100

    adding home headlines and image

--- paark_frontend/src/index.js
+++ paark_frontend/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import Home from "./views/Home/Home";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- paark_frontend/src/views/Home/Home.js
+++ paark_frontend/src/views/Home/Home.js
@@ -1,8 +1,27 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import paark_home from "../../assets/images/paark_home.jpg";
 import "./Home.scss";
-import Navigation from "../../components/Navigation/Navigation";
 
 const Home = () => {
-  return <Navigation />;
+  return (
+    <Container className="home">
+      <Row className="home__headlines">
+        <Row className="home__headlines__headline">
+          Service de voiturier à la demande.
+        </Row>
+        <Row className="home__headlines__sub-headline">
+          Utilisez Paark pour le travail, la maison ou les loisirs : rapide,
+          abordable et pratique.
+        </Row>
+      </Row>
+      <Row className="home__image">
+        <Image className="home__image__paark-home" src={paark_home} rounded />
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
