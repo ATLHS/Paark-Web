b9615f61a4e705dcb7bf6e24424135c50d6339e0
ATLH <s-attilah@hotmail.com>
2022-03-17 21:34:03 +0100

    adding aws config for deployement on ec2 instance

---
+++ aws-script/mvbuildreactfolder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+sudo chown -R ubuntu "/home/paark-web/"
+
+# Building react app
+cd "/home/paark-web/paark-frontend/"
+npm run build
+
+# Remove react build folder content from Nginx web server to new location
+sudo rm -r "/var/www/paark-web/"*
+
+# Copy paste new react build folder to nginx web server new location
+cp -r "/home/paark-web/paark-frontend/build/"* "/var/www/paark-web"
+
+# Installing backend dependencies
+cd "/home/paark-web/paark-backend/"
+npm i
+
+# Installing frontend dependencies
+cd "/home/paark-web/paark-frontend/"
+npm i
+
+# Restart Express server and Nginx
+
+# cd "/home/paark-web/paark-backend/"
+pm2 restart server.js
+
+# cd "/home/"
+sudo systemctl restart nginx
+ 
