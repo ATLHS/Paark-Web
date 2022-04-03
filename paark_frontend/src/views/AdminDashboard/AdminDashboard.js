9c7b68b9f675691730a21dda93570e1ef2cdf6f9
ATLH <s-attilah@hotmail.com>
2022-04-03 03:31:38 +0200

    responsive admin dahsboard

--- paark_frontend/src/views/AdminDashboard/AdminDashboard.js
+++ paark_frontend/src/views/AdminDashboard/AdminDashboard.js
@@ -9,10 +9,15 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import Nav from "react-bootstrap/Nav";
 import rideServices from "../../services/ride";
 import userServices from "../../services/user";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
 import "./AdminDashboard.scss";
 
 const AdminDashboard = () => {
   // const { user } = useContext(AuthApi);
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   const [initialUsers, setInitialUser] = useState([]);
   const [statusesLength, setStatusesLength] = useState({});
@@ -166,121 +171,253 @@ const AdminDashboard = () => {
   return (
     <Container className="admin-dashboard">
       <Row className="admin-dashboard__container">
-        <Col className="admin-dashboard__container__table">
-          <Row className="admin-dashboard__container__table__nav">
-            <Nav className="admin-dashboard__container__table__nav__base-nav">
-              <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
-                <Nav.Link
-                  onClick={getAllOptions}
-                  className="admin-dashboard__container__table__nav__base-nav__item"
-                >
-                  Tous {allStatusLength()}
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
-                <Nav.Link
-                  onClick={users ? getResgisteredOptions : null}
-                  className="admin-dashboard__container__table__nav__base-nav__item"
-                >
-                  Enregistré {statusesLength[constStatus.REGISTERED]}
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
-                <Nav.Link
-                  onClick={users ? getOnGoingOptions : null}
-                  className="admin-dashboard__container__table__nav__base-nav__item"
-                >
-                  En chemin {statusesLength[constStatus.ONGOING]}
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
-                <Nav.Link
-                  onClick={users ? getPickedupOptions : null}
-                  className="admin-dashboard__container__table__nav__base-nav__item"
-                >
-                  Pris en charge {statusesLength[constStatus.PICKEDUP]}
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
-                <Nav.Link
-                  onClick={users ? getReturnedOptions : null}
-                  className="admin-dashboard__container__table__nav__base-nav__item"
-                >
-                  Terminée {statusesLength[constStatus.RETURNED]}
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Row>
-          <Row className="admin-dashboard__container__table__header">
-            <Row className="admin-dashboard__container__table__header__tabs">
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                PRÉNOM
-              </Col>
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                ADRESSE D'ARRIVÉE
-              </Col>
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                HUERE D'ARRIVÉ
-              </Col>
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                ADERESSE DE RETOUR
-              </Col>
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                NUMÉRO DE TÉLÉPHONE
-              </Col>
-              <Col className="admin-dashboard__container__table__header__tabs__tab">
-                STATUS
-              </Col>
+        {!isMobile ? (
+          <Col className="admin-dashboard__container__table">
+            <Row className="admin-dashboard__container__table__nav">
+              <Nav className="admin-dashboard__container__table__nav__base-nav">
+                <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
+                  <Nav.Link
+                    onClick={getAllOptions}
+                    className="admin-dashboard__container__table__nav__base-nav__item"
+                  >
+                    Tous {allStatusLength()}
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
+                  <Nav.Link
+                    onClick={users ? getResgisteredOptions : null}
+                    className="admin-dashboard__container__table__nav__base-nav__item"
+                  >
+                    Enregistré {statusesLength[constStatus.REGISTERED]}
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
+                  <Nav.Link
+                    onClick={users ? getOnGoingOptions : null}
+                    className="admin-dashboard__container__table__nav__base-nav__item"
+                  >
+                    En chemin {statusesLength[constStatus.ONGOING]}
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
+                  <Nav.Link
+                    onClick={users ? getPickedupOptions : null}
+                    className="admin-dashboard__container__table__nav__base-nav__item"
+                  >
+                    Pris en charge {statusesLength[constStatus.PICKEDUP]}
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="admin-dashboard__container__table__nav__base-nav__items">
+                  <Nav.Link
+                    onClick={users ? getReturnedOptions : null}
+                    className="admin-dashboard__container__table__nav__base-nav__item"
+                  >
+                    Terminée {statusesLength[constStatus.RETURNED]}
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
             </Row>
-          </Row>
-          {!isLoading && users
-            ? users.map((user) => (
-                <Row
-                  key={user._id}
-                  className="admin-dashboard__container__table__body"
-                >
-                  <Row className="admin-dashboard__container__table__body__race">
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      {user.firstname}
-                    </Col>
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      {user.rides[0].dropOffLocation}
-                    </Col>
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      {user.rides[0].dropOffTime}
-                    </Col>
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      {user.rides[0].dropBackLocation || "-"}
-                    </Col>
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      {user.phone}
-                    </Col>
-                    <Col className="admin-dashboard__container__table__body__race__info">
-                      <DropdownButton
-                        className={`admin-dashboard__container__table__body__race__info__selector-status ${
-                          user.rides[0].status === constStatus.RETURNED ||
-                          user.rides[0].status === constStatus.REGISTERED
-                            ? "hide"
-                            : ""
-                        }`}
-                        title={user.rides[0].status}
-                        variant={getVariantStatus(user.rides[0].status)}
-                        disabled={
-                          user.rides[0].status === constStatus.RETURNED ||
-                          user.rides[0].status === constStatus.REGISTERED
-                        }
-                        onSelect={(eventKey) =>
-                          setTitle(user.rides[0].status, eventKey, user._id)
-                        }
-                      >
-                        {getOptions(user)}
-                      </DropdownButton>
-                    </Col>
+            <Row className="admin-dashboard__container__table__header">
+              <Row className="admin-dashboard__container__table__header__tabs">
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  PRÉNOM
+                </Col>
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  ADRESSE D'ARRIVÉE
+                </Col>
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  HUERE D'ARRIVÉ
+                </Col>
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  ADERESSE DE RETOUR
+                </Col>
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  NUMÉRO DE TÉLÉPHONE
+                </Col>
+                <Col className="admin-dashboard__container__table__header__tabs__tab">
+                  STATUS
+                </Col>
+              </Row>
+            </Row>
+            {!isLoading && users
+              ? users.map((user) => (
+                  <Row
+                    key={user._id}
+                    className="admin-dashboard__container__table__body"
+                  >
+                    <Row className="admin-dashboard__container__table__body__race">
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        {user.firstname}
+                      </Col>
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        {user.rides[0].dropOffLocation}
+                      </Col>
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        {user.rides[0].dropOffTime}
+                      </Col>
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        {user.rides[0].dropBackLocation || "-"}
+                      </Col>
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        {user.phone}
+                      </Col>
+                      <Col className="admin-dashboard__container__table__body__race__info">
+                        <DropdownButton
+                          className={`admin-dashboard__container__table__body__race__info__selector-status ${
+                            user.rides[0].status === constStatus.RETURNED ||
+                            user.rides[0].status === constStatus.REGISTERED
+                              ? "hide"
+                              : ""
+                          }`}
+                          title={user.rides[0].status}
+                          variant={getVariantStatus(user.rides[0].status)}
+                          disabled={
+                            user.rides[0].status === constStatus.RETURNED ||
+                            user.rides[0].status === constStatus.REGISTERED
+                          }
+                          onSelect={(eventKey) =>
+                            setTitle(user.rides[0].status, eventKey, user._id)
+                          }
+                        >
+                          {getOptions(user)}
+                        </DropdownButton>
+                      </Col>
+                    </Row>
                   </Row>
-                </Row>
-              ))
-            : ""}
-        </Col>
+                ))
+              : ""}
+          </Col>
+        ) : (
+          <>
+            <Accordion className="admin-dashboard__container__filter" flush>
+              <Accordion.Item className="admin-dashboard__container__filter__items">
+                <Accordion.Header className="admin-dashboard__container__filter__items__header">
+                  Filtres
+                </Accordion.Header>
+                <Accordion.Body>
+                  <Button
+                    className="m-1"
+                    variant="outline-dark"
+                    size="sm"
+                    onClick={getAllOptions}
+                  >
+                    Tous {allStatusLength()}
+                  </Button>
+                  <Button
+                    className="m-1"
+                    variant="outline-dark"
+                    size="sm"
+                    onClick={users ? getResgisteredOptions : null}
+                  >
+                    Enregistré {statusesLength[constStatus.REGISTERED]}
+                  </Button>
+                  <Button
+                    className="m-1"
+                    variant="outline-dark"
+                    size="sm"
+                    onClick={users ? getOnGoingOptions : null}
+                  >
+                    En chemin {statusesLength[constStatus.ONGOING]}
+                  </Button>
+                  <Button
+                    className="m-1"
+                    variant="outline-dark"
+                    size="sm"
+                    onClick={users ? getPickedupOptions : null}
+                  >
+                    Pris en charge {statusesLength[constStatus.PICKEDUP]}
+                  </Button>
+                  <Button
+                    className="m-1"
+                    variant="outline-dark"
+                    size="sm"
+                    onClick={users ? getReturnedOptions : null}
+                  >
+                    Terminée {statusesLength[constStatus.RETURNED]}
+                  </Button>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            {!isLoading && users
+              ? users.map((user) => (
+                  <Col
+                    key={user._id}
+                    className="admin-dashboard__container__mobile"
+                  >
+                    <Card
+                      className="admin-dashboard__container__mobile__card"
+                      style={{ width: "18rem" }}
+                    >
+                      <Card.Body className="admin-dashboard__container__mobile__card__body">
+                        <Card.Title className="admin-dashboard__container__mobile__card__body__title">
+                          {user.firstname}
+                        </Card.Title>
+                        <Row className="admin-dashboard__container__mobile__card__body__container">
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__drop-off">
+                            Dépôt :
+                          </Card.Text>
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__address">
+                            {user.rides[0].dropOffLocation}
+                          </Card.Text>
+                        </Row>
+                        <Row className="admin-dashboard__container__mobile__card__body__container">
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__drop-off__hour">
+                            Heure d'arrivé :
+                          </Card.Text>
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__hour">
+                            {user.rides[0].dropOffTime}
+                          </Card.Text>
+                        </Row>
+                        <Row className="admin-dashboard__container__mobile__card__body__container">
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__drop-off">
+                            Retour :
+                          </Card.Text>
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__address">
+                            {user.rides[0].dropBackLocation || "-"}
+                          </Card.Text>
+                        </Row>
+                        <Row className="admin-dashboard__container__mobile__card__body__container">
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__phone">
+                            Tél :
+                          </Card.Text>
+                          <Card.Text className="admin-dashboard__container__mobile__card__body__container__number">
+                            {user.phone}
+                          </Card.Text>
+                        </Row>
+                        <Row className="admin-dashboard__container__mobile__card__body__actions">
+                          <Col className="admin-dashboard__container__mobile__card__body__action">
+                            <DropdownButton
+                              className={`admin-dashboard__container__mobile__card__body__action__selector-status ${
+                                user.rides[0].status === constStatus.RETURNED ||
+                                user.rides[0].status === constStatus.REGISTERED
+                                  ? "hide"
+                                  : ""
+                              }`}
+                              title={user.rides[0].status}
+                              variant={getVariantStatus(user.rides[0].status)}
+                              disabled={
+                                user.rides[0].status === constStatus.RETURNED ||
+                                user.rides[0].status === constStatus.REGISTERED
+                              }
+                              onSelect={(eventKey) =>
+                                setTitle(
+                                  user.rides[0].status,
+                                  eventKey,
+                                  user._id
+                                )
+                              }
+                            >
+                              {getOptions(user)}
+                            </DropdownButton>
+                          </Col>
+                        </Row>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))
+              : ""}
+          </>
+        )}
       </Row>
     </Container>
   );
