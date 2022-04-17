92f6235ff94173142e914570bbe2145abaa023fc
ATLH <s-attilah@hotmail.com>
2022-04-17 03:26:37 +0200

    adding follow us on instagram icon

--- paark_frontend/src/components/Footer/Footer.js
+++ paark_frontend/src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./Footer.scss";
 
 const Footer = () => {
@@ -10,15 +11,23 @@ const Footer = () => {
       <Container className="footer">
         <Row className="footer__socials">
           <Row className="footer__socials__follow-us">Suivez-nous !</Row>
-          <Row className="footer__socials__twitter">
-            <a href="https://twitter.com/paark_fr" target="blank">
-              <FontAwesomeIcon
-                as="a"
+          <Row className="footer__socials__section">
+            <Col className="footer__socials__section__icons">
+              <a
+                className="footer__socials__section__icons__twitter"
                 href="https://twitter.com/paark_fr"
-                icon={faTwitter}
-                size="2x"
-              />
-            </a>
+                target="blank"
+              >
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+              <a
+                className="footer__socials__section__icons__instagram"
+                href="https://www.instagram.com/paark_fr/?hl=fr"
+                target="blank"
+              >
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+            </Col>
           </Row>
         </Row>
         <Row className="footer__copyright">
