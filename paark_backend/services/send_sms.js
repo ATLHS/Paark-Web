38fe8c69727322bf3fe48b0c25943898e2aaba24
ATLH <s-attilah@hotmail.com>
2022-04-24 20:02:15 +0200

    convert commonJS module to ESmodule

--- paark_backend/services/send_sms.js
+++ paark_backend/services/send_sms.js
@@ -1,10 +1,15 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
-const smsNotification = require("../utils/smsNotificationMssage");
+import {
+  confirmationCodeMessage,
+  userConfirmedReservationMessage,
+  userConfirmedCarBackMessage,
+  adminNewReservationAlertMessage,
+} from "../utils/smsNotificationMssage";
 
 const confirmationCodeOptions = (code, phone) => ({
-  body: smsNotification.confirmationCodeMessage(code),
+  body: confirmationCodeMessage(code),
   from: process.env.PAARK,
   to: `+33${phone}`,
 });
@@ -15,52 +20,49 @@ const userConfirmedReservationOptions = (
   address,
   dropOffCode
 ) => ({
-  body: smsNotification.userConfirmedReservationMessage(
-    hour,
-    address,
-    dropOffCode
-  ),
+  body: userConfirmedReservationMessage(hour, address, dropOffCode),
   from: process.env.PAARK,
   to: `+33${phone}`,
 });
 
 const userConfirmedCarBackOptions = (address, dropBackCode, phone) => ({
-  body: smsNotification.userConfirmedCarBackMessage(address, dropBackCode),
+  body: userConfirmedCarBackMessage(address, dropBackCode),
   from: process.env.PAARK,
   to: `+33${phone}`,
 });
 
 const adminNewReservationAlertOptions = () => ({
-  body: smsNotification.adminNewReservationAlertMessage(),
+  body: adminNewReservationAlertMessage(),
   from: process.env.PAARK_RESERVATION,
   to: `+33${process.env.VALET_NUMBER}`,
 });
 
-module.exports = {
-  sendConfirmationCodeSms: (code, phone) => {
-    client.messages.create(confirmationCodeOptions(code, phone)).then((res) => {
+export function sendConfirmationCodeSms(code, phone) {
+  client.messages.create(confirmationCodeOptions(code, phone)).then((res) => {
+    return res.status === "sent";
+  });
+}
+export function sendUserConfirmedReservationSms(
+  phone,
+  hour,
+  address,
+  dropOffCode
+) {
+  client.messages
+    .create(userConfirmedReservationOptions(phone, hour, address, dropOffCode))
+    .then((res) => {
       return res.status === "sent";
     });
-  },
-  sendUserConfirmedReservationSms: (phone, hour, address, dropOffCode) => {
-    client.messages
-      .create(
-        userConfirmedReservationOptions(phone, hour, address, dropOffCode)
-      )
-      .then((res) => {
-        return res.status === "sent";
-      });
-  },
-  sendUserConfirmedCarBackSms: (address, dropBackCode, phone) => {
-    client.messages
-      .create(userConfirmedCarBackOptions(address, dropBackCode, phone))
-      .then((res) => {
-        return res.status === "sent";
-      });
-  },
-  sendAdminNewReservationAlertSms: () => {
-    client.messages.create(adminNewReservationAlertOptions()).then((res) => {
+}
+export function sendUserConfirmedCarBackSms(address, dropBackCode, phone) {
+  client.messages
+    .create(userConfirmedCarBackOptions(address, dropBackCode, phone))
+    .then((res) => {
       return res.status === "sent";
     });
-  },
-};
+}
+export function sendAdminNewReservationAlertSms() {
+  client.messages.create(adminNewReservationAlertOptions()).then((res) => {
+    return res.status === "sent";
+  });
+}
